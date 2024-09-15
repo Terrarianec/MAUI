b027c3970a6bdf9166fe75be44c40149bd4901ad
Terrarianec <dimaryazantcev@gmail.com>
2024-09-15 14:35:53 +0300

    Добавьте файлы проекта.

---
+++ Reminder/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace Reminder
+{
+	public partial class MainPage : ContentPage
+	{
+		private readonly IDispatcherTimer _timer = Application.Current!.Dispatcher.CreateTimer();
+
+		public MainPage()
+		{
+			InitializeComponent();
+
+			_timer.Interval = TimeSpan.FromSeconds(1);
+			_timer.Tick += OnTick;
+			_timer.Start();
+		}
+
+		private void OnTick(object? sender, EventArgs e)
+		{
+			if (Math.Abs((DateTime.Now - datePicker.Date.Add(timePicker.Time)).Ticks) < _timer.Interval.Ticks / 2)
+				DisplayAlert("Напоминалка", $"{datePicker.Date.Add(timePicker.Time):dd.MM.yyyy HH:mm}\n{messageEntry.Text}", "Ладно");
+		}
+	}
+
+}
