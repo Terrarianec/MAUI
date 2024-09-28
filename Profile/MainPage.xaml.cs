80b4aa82e27c2761cbf136cb3e584fad3edd6df6
Terrarianec <dimaryazantcev@gmail.com>
2024-09-28 23:16:52 +0300

    Добавьте файлы проекта.

---
+++ Profile/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿namespace Profile;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnPageLoaded(object sender, EventArgs e)
+	{
+		dateOfBirthPicker.MaximumDate = DateTime.Today - TimeSpan.FromDays(365 * 16);
+		dateOfBirthPicker.MinimumDate = DateTime.MinValue;
+
+		var images = new List<string> {
+			"image_00009_neuro.png",
+			"image_00011_neuro.png",
+			"image_00014_neuro.png",
+			"image_00018_neuro.png",
+			"image_00025_neuro.png",
+			"image_00026_neuro.png",
+			"image_00047_neuro.png"
+		}.Select(path => ImageSource.FromFile(path)).ToList();
+		imagePathPicker.ItemsSource = images;
+	}
+
+	private void OnDateOfBirthSelected(object sender, DateChangedEventArgs e)
+	{
+		var age = DateTime.Now.Year - e.NewDate.Year;
+
+		if (DateTime.Now.DayOfYear < e.NewDate.DayOfYear)
+			age--;
+
+		ageLabel.Text = age.ToString("0 лет");
+	}
+
+	private void OnAddMarkButtonClick(object sender, EventArgs e)
+	{
+		var label = new Label { VerticalOptions = LayoutOptions.CenterAndExpand };
+		var stepper = new Stepper { Minimum = 2, Maximum = 5, Increment = 1 };
+
+		label.SetBinding(Label.TextProperty, new Binding { Source = stepper, Path = "Value", StringFormat = "Оценка: {0}" });
+
+		var layout = new StackLayout { Orientation = StackOrientation.Horizontal, Spacing = 20 };
+		layout.Children.Add(label);
+		layout.Children.Add(stepper);
+
+		var cell = new ViewCell { View = layout };
+
+		marksSection.Add(cell);
+	}
+}
